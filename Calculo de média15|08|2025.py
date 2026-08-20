85c24fd2834d801289e21a3e95a3281f851a6476
Bernardo Rezende <bernardorezende19@gmail.com>
2026-08-20 13:25:37 -0300

    Create Calculo de média15|08|2025.py

---
+++ Calculo de média15|08|2025.py
@@ -0,0 +1,12 @@
+notaAV = 7
+press = 80
+notaAVS = 7
+FilhoDePolitico = False
+if(notaAV>6 & press>= 75):
+    print("Passou")
+elif(notaAVS>=6):
+    print("Passou")
+elif(FilhoDePolitico == True):
+    print("Passou")
+else:
+    print("Reprovado")
